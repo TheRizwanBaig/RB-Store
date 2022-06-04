e8fee66949844a6e1a6982bb8ef5958aff519d39
TheRizwanBaig <therizwanbaig@gmail.com>
2022-06-04 20:20:45 +0500

    update

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -18,44 +18,48 @@ const Cart = () => {
   });
   return (
     <>
-    <div className="cart__h">
-      <h1>Cart</h1>
-    </div>
-    <div className="cart__container">
-      <div className="cart__details">
-        <div className="cart__image">
-          <img src={cartProducts.image} />
-        </div>
-        <div className="cart__box">
-          <h3>{cartProducts.title}</h3>
-          <div className="cart__row">
-            <div className="btn__container">
-              <div className="inc__dec__btn">
-                <button onClick={() => setProductQuantity(productQuantity - 1)}>
-                  -
-                </button>
-                <div className="product__quantity">
-                  <h3>{productQuantity}</h3>
+      <div className="cart__h">
+        <h1>Cart</h1>
+      </div>
+      <div className="cart__container">
+        <div className="cart__details">
+          <div className="cart__image">
+            <img src={cartProducts.image} />
+          </div>
+          <div className="cart__box">
+            <h3>{cartProducts.title}</h3>
+            <div className="cart__row">
+              <div className="btn__container">
+                <div className="inc__dec__btn">
+                  <button
+                    onClick={() => setProductQuantity(productQuantity - 1)}
+                  >
+                    -
+                  </button>
+                  <div className="product__quantity">
+                    <h3>{productQuantity}</h3>
+                  </div>
+                  <button
+                    onClick={() => setProductQuantity(productQuantity + 1)}
+                  >
+                    +
+                  </button>
                 </div>
-                <button onClick={() => setProductQuantity(productQuantity + 1)}>
-                  +
+              </div>
+              <div className="del__btn">
+                <button onClick={() => setProductQuantity(0)}>
+                  <DeleteIcon />
                 </button>
               </div>
             </div>
-            <div className="del__btn">
-              <button onClick={() => setProductQuantity(0)}>
-                <DeleteIcon />
-              </button>
-            </div>
           </div>
+          <span>${cartProducts.price}</span>
         </div>
-        <span>${cartProducts.price}</span>
       </div>
-    </div>
       <div className="total">
-        <h2>SubTotal ${productQuantity*cartProducts.price}</h2>
+        <h2>SubTotal ${productQuantity * cartProducts.price}</h2>
       </div>
-   </>
+    </>
   );
 };
 
